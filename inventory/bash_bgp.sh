c6b0f499339aead3becc4ae4f9c5f23f2ada4ce6
ipubrich <rich.tornambe@hotmail.co.uk>
2021-07-10 08:03:49 +0100

    updated yaml

--- inventory/bash_bgp.sh
+++ inventory/bash_bgp.sh
@@ -9,7 +9,7 @@
 # Variables
 INVENTORY='devices.json'
 TEMP_DIR='/tmp/bash_bgp'
-RESULT_DIR='output/'
+RESULT_DIR='output'
 
 # User-defined functions
 
@@ -35,59 +35,55 @@ case ${3} in
         COMMAND="net show bgp summary"
         ;;
 esac
-#set timeout -1
-#spawn ssh ${USERNAME}@${2}
-#echo " ${USERNAME}@${2}"
-#expect "Password"
-#send --"${PASSWORD}\r"
 
-
-echo "Collecting interfaces status for ${1} ... "
+echo "Collecting BGP status for ${1} ... "
 touch "${TEMP_DIR}/${1}_raw_data.txt"
-echo "command : ${COMMAND}  user : ${USERNAME} IP ${2}"
+echo "command : ${COMMAND}  user : ${USERNAME} IP : ${2}"
 ssh ${USERNAME}@${2} "${COMMAND}" > "${TEMP_DIR}/${1}_raw_data.txt"
 }
 
 function parse_data() {
-echo "${1}" "${3}"
-touch "${TARGET_DIR}/${1}_results.yaml"
+echo "Parsing : ${1} ${2}"
+touch "${RESULT_DIR}/${1}_result.yaml"
+echo "${RESULT_DIR}/${1}_result.yaml"
 echo "---" > "${RESULT_DIR}/${1}_result.yaml"
 echo "bgp:" >> "${RESULT_DIR}/${1}_result.yaml"
 echo " address_family:" >> "${RESULT_DIR}/${1}_result.yaml"
-echo " - afi: ipv4" > "${RESULT_DIR}/${1}_result.yaml"
-echo " safi: unicast" >> "${RESULT_DIR}/${1}_result.yaml"
-echo " neighbors:" >> "${RESULT_DIR}/${1}_result.yaml"
+echo "   - afi: ipv4" >> "${RESULT_DIR}/${1}_result.yaml"
+echo "     safi: unicast" >> "${RESULT_DIR}/${1}_result.yaml"
+echo "     neighbors:" >> "${RESULT_DIR}/${1}_result.yaml"
 
-case ${3} in
-    'Cisco XR-V')
-        NEIGHBOR_IP=$(echo "${LINE}" | awk '/^[0-9]/ {print $1}')
-        NEIGHBOR_ASN=$(echo "${LINE}" | awk '/^[0-9]/ {print $3}')
-        NEIGHBOR_STATE=$(echo "${LINE}" | awk '/^[0-9]/ {print $10}')
-        ;;
-    'Arista EOS')
-        NEIGHBOR_IP=$(echo "${LINE}" | awk '/^ [0-9]/ {print 1}')
-        NEIGHBOR_ASN=$(echo "${LINE}" | awk '/^ [0-9]/ {print 1}')
-        NEIGHBOR_STATE=$(echo "${LINE}" | awk '/^ [0-9]/ {print 1}')
-        ;;
-    'Cumulus Linux')
-        NEIGHBOR_IP=$(echo "${LINE}" | awk '/^[0-9]/ {print 1}')
-        NEIGHBOR_ASN=$(echo "${LINE}" | awk '/^[0-9]/ {print 3}')
-        NEIGHBOR_STATE=$(echo "${LINE}" | awk '/^[0-9]/ {print 10}')
-        ;;
-        
-esac
 
-if [[ ${NEIGHBOR_IP} != '' ]]; then
-    echo " - neighbor: ${NEIGHBOR_IP}" >> "${RESULT_DIR}/${1}_result.yaml"
-    echo " peer_as: ${NEIGHBOR_ASN}" >> "${RESULT_DIR}/${1}_result.yaml"
-    echo " state: ${NEIGHBOR_STATE}" >> "${RESULT_DIR}/${1}_result.yaml"
-    else
-    :
-fi
 
-while read LINE; do
-    echo "${LINE}"
-done < "${TEMP_DIR}/${1}_raw.txt"
+input="${TEMP_DIR}/${1}_raw_data.txt"
+while read -r LINE; do
+
+    case ${2} in
+        'Cisco XR-V')
+            NEIGHBOR_IP=$(echo "${LINE}" | awk '/^[0-9]/ {print $1}')
+            NEIGHBOR_ASN=$(echo "${LINE}" | awk '/^[0-9]/ {print $3}')
+            NEIGHBOR_STATE=$(echo "${LINE}" | awk '/^[0-9]/ {print $10}');;
+        'Arista EOS')
+            NEIGHBOR_IP=$(echo "${LINE}" | awk '/^ [0-9]/ {print 1}')
+            NEIGHBOR_ASN=$(echo "${LINE}" | awk '/^ [0-9]/ {print 1}')
+            NEIGHBOR_STATE=$(echo "${LINE}" | awk '/^ [0-9]/ {print 1}');;
+        'Cumulus Linux')
+            NEIGHBOR_IP=$(echo "${LINE}" | awk '/^[0-9]/ {print $1}')
+            NEIGHBOR_ASN=$(echo "${LINE}" | awk '/^[0-9]/ {print $3}')
+            NEIGHBOR_STATE=$(echo "${LINE}" | awk '/^[0-9]/ {print $10}');;
+    esac
+    if [[ ${NEIGHBOR_IP} != '' ]]; then
+        echo "LINE HERE ${LINE}"
+        echo "${NEIGHBOR_IP}"
+        echo "      - neighbor: ${NEIGHBOR_IP}" >> "${RESULT_DIR}/${1}_result.yaml"
+        echo "        peer_as: ${NEIGHBOR_ASN}" >> "${RESULT_DIR}/${1}_result.yaml"
+        echo "        state: ${NEIGHBOR_STATE}" >> "${RESULT_DIR}/${1}_result.yaml"
+        else
+        :
+    fi
+    
+done < "$input"
+
 echo "..." >> "${RESULT_DIR}/${1}_result.yaml"
 }
 
@@ -106,15 +102,15 @@ if  [[ ${HOST} != 'null' ]]; then
     echo "JSON FOUND - ${HOST} ${IP} ${NOS}"
         if [[ ! -d "${TEMP_DIR}" ]]; then
         echo "Creating temp dir ${TEMP_DIR}"
-        mkdir "${TEMP_DIR}"
-        mkdir "${RESULT_DIR}"
+        sudo mkdir "${TEMP_DIR}"
+        sudo mkdir "${RESULT_DIR}"
         else
         :
         fi
     
-    collect_data "${HOST}" "${IP}" "${NOS}"
-    parse_data "${HOST}" "${NOS}"
-
+    collect_data "${HOST}" "${IP}" "${NOS}" # take outputs and pipe to a file
+    parse_data "${HOST}" "${NOS}" # parse into yaml
+    echo ""
     else
     :
     fi
